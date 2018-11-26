40f05f3f76b67bf5ec1ca171bc790e6656990184
AshKai <yaimesil@gmail.com>
2018-11-26 12:20:55 +0000

    update

--- bloodcomp.py
+++ bloodcomp.py
@@ -2,15 +2,17 @@ from itertools import combinations
 
 # Dictionary with compatible blood types:
 # A = A+ | Z = A- | B = B+ | Y = B- | C = AB+ | X = AB- | O = O+ | W = O-
-b_comp = {"A" : ["A", "C"],
-          "Z" : ["A", "Z", "C", "X" ],
-          "B" : ["B", "C"],
-          "Y" : ["B", "Y", "C", "X"],
-          "C" : ["C"],
-          "X" : ["C", "X"],
-          "O" : ["A", "B", "C", "O"],
-          "W" : ["A", "Z", "B", "Y", "C", "X", "O", "W"]}
+b_comp = {"A": ["A", "C"],
+          "Z": ["A", "Z", "C", "X"],
+          "B": ["B", "C"],
+          "Y": ["B", "Y", "C", "X"],
+          "C": ["C"],
+          "X": ["C", "X"],
+          "O": ["A", "B", "C", "O"],
+          "W": ["A", "Z", "B", "Y", "C", "X", "O", "W"]}
 last_patient = ""
+total = 0
+
 
 # Checks blood compatibility between two persons
 def check_compatibility(patient1_blood, patient2_blood):
@@ -19,22 +21,27 @@ def check_compatibility(patient1_blood, patient2_blood):
             return True
     return False
 
+
 # Gets the list of all compatible persons
 def get_compatibility(combination):
     patient1 = combination[0]
     patient2 = combination[1]
 
-    p1_blood = patient1[len(patient1)-1]
-    p2_blood = patient2[len(patient2)-1]
+    p1_blood = patient1[len(patient1) - 1]
+    p2_blood = patient2[len(patient2) - 1]
 
     global last_patient
     if check_compatibility(p1_blood, p2_blood):
         if last_patient != patient1:
             last_patient = patient1
             print("\nPatient", patient1, "can donate to:")
-        print(patient2)
+        print(patient2, "\n")
+
 
 # Gets all combinations without repetition of the given persons
 def get_combinations(persons):
+    global total
     for comb in combinations(persons, 2):
+        total = total + 1
         get_compatibility(comb)
+    print("Total combinations:", total)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import time
 import sys
 import bloodcomp
+import prim
 
 if len(sys.argv) < 3:
     print("usage: main.py <dataset_file> <-b | -g | -p> [-t]")
@@ -11,29 +12,30 @@ if len(sys.argv) < 3:
           "   -t, Execution with time")
 
 if len(sys.argv) == 3:
-    if sys.argv[3] == "-b":
+    if sys.argv[2] == "-b":
         persons = open(sys.argv[1], "r").read().split("\n")
         bloodcomp.get_combinations(persons)
-    if sys.argv[3] == "-g":
-        persons_sorted = sorted(persons, key=lambda x: x.split(":")[-1])
+    if sys.argv[2] == "-g":
+        persons_sorted = sorted(open(sys.argv[1], "r").read().split("\n"), key=lambda x: x.split(":")[-1])
         bloodcomp.get_combinations(persons_sorted)
-    if sys.argv[3] == "-p":
-        print()
+    if sys.argv[2] == "-p":
+        graph = open(sys.argv[1], "r").read().split("\n")
+        prim.init(graph)
 
-if sys.argv[3] == "-t":
-    if sys.argv[3] == "-b":
+if len(sys.argv) == 4 and sys.argv[3] == "-t":
+    if sys.argv[2] == "-b":
         startTime = time()
         persons = open(sys.argv[1], "r").read().split("\n")
         bloodcomp.get_combinations(persons)
         elapsedTime = time() - startTime
         print("\nTotal execution time:", elapsedTime * 1000, "milliseconds")
-    if sys.argv[3] == "-g":
+    if sys.argv[2] == "-g":
         startTime = time()
-        persons_sorted = sorted(persons, key=lambda x: x.split(":")[-1])
+        persons_sorted = sorted(open(sys.argv[1], "r").read().split("\n"), key=lambda x: x.split(":")[-1])
         bloodcomp.get_combinations(persons_sorted)
         elapsedTime = time() - startTime
         print("\nTotal execution time:", elapsedTime * 1000, "milliseconds")
-    if sys.argv[3] == "-p":
+    if sys.argv[2] == "-p":
         startTime = time()
 
         elapsedTime = time() - startTime
